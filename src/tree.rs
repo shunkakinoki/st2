4102f9d080699e0743b2a52e190d1c9d9362fa41
Shun Kakinoki <shunkakinoki@gmail.com>
2024-10-31 05:32:08 +0900

    chore: update

--- src/tree.rs
+++ src/tree.rs
@@ -209,7 +209,7 @@ impl TrackedBranch {
 #[serde(rename_all = "kebab-case")]
 pub struct RemoteMetadata {
     /// The name of the remote. Defaults to "origin" if not provided.
-    pub(crate) name: Option<String>,
+    pub(crate) remote_name: Option<String>,
     /// The number of the pull request on GitHub associated with the branch.
     pub(crate) pr_number: u64,
     /// The comment ID of the stack status comment on the pull request.
@@ -221,9 +221,9 @@ pub struct RemoteMetadata {
 
 impl RemoteMetadata {
     /// Creates a new [RemoteMetadata] with the given PR number and comment ID.
-    pub fn new(name: Option<String>, pr_number: u64) -> Self {
+    pub fn new(remote_name: Option<String>, pr_number: u64) -> Self {
         Self {
-            name,
+            remote_name,
             pr_number,
             comment_id: None,
         }
